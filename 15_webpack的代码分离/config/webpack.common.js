6cca621861d005227a31d657f95ad7c9a64a78cf
six <luzilong@mininglamp.com>
2022-01-17 15:53:11 +0800

    手动多入口分离代码

---
+++ 15_webpack的代码分离/config/webpack.common.js
@@ -0,0 +1,59 @@
+const resolveApp = require("./paths");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+const { merge } = require("webpack-merge");
+
+const prodConfig = require("./webpack.prod");
+const devConfig = require("./webpack.dev");
+
+const commonConfig = {
+  // 多入口
+  entry: {
+    main: './src/main.js',
+    index: './src/index.js'
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: resolveApp("./build"),
+  },
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx", ".ts", ".vue"],
+    alias: {
+      "@": resolveApp("./src"),
+      pages: resolveApp("./src/pages"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        use: "babel-loader",
+      },
+      {
+        test: /\.vue$/i,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new VueLoaderPlugin(),
+  ]
+};
+
+module.exports = function(env) {
+  const isProduction = env.production;
+  process.env.NODE_ENV = isProduction ? "production": "development";
+
+  const config = isProduction ? prodConfig : devConfig;
+  const mergeConfig = merge(commonConfig, config);
+
+  return mergeConfig;
+};
