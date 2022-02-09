a197adc725d71337f211fce67caccb98bef8dc42
six <luzilong@mininglamp.com>
2022-02-09 14:34:46 +0800

    添加注释

--- 20_library/webpack.config.js
+++ 20_library/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./build"),
     filename: "lib.js",
+    // 打包出来的格式即支持amd和cmd规范
     libraryTarget: "umd",
     library: "lib",
     globalObject: "this"
